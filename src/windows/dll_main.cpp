ecd66c206344fef0a39e7ede4eeb30e38ca45fcf
Gnil <your_email@example.com>
2026-09-23 15:40:20 +0300

    Изменение названия файла.

--- src/windows/dll_main.cpp
+++ src/windows/dll_main.cpp
@@ -1,7 +1,7 @@
-#include "launcher.h"
+#include "launcher.hpp"
 #include "../shared/core.hpp"
 
-extern "C" LAUNCHER_API int RunApp() {
+extern "C" LAUNCHER_API inline int RunApp() {
     return core::app();
 }
 
