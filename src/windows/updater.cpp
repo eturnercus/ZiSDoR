b9d82c81136aad719b81c4e2b295a38c3041df7a
Gnil <your_email@example.com>
2026-09-24 21:26:08 +0300

    Убрал allocconsole

--- src/windows/updater.cpp
+++ src/windows/updater.cpp
@@ -3,11 +3,6 @@
 #include "launcher.hpp"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Принудительно создаем консоль для вывода std::cout в WinMain
-    AllocConsole();
-    FILE* f;
-    freopen_s(&f, "CONOUT$", "w", stdout);
-
     std::cout << "[Updater] Starting..." << std::endl;
 
     std::cout << "[Updater] Loading launcher.dll..." << std::endl;
